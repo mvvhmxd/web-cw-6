9e79d97a0da79522ba1fcd5fce29dc14b998262f
Mahmoud Mohamed Balash <mbh27176@gmail.com>
2022-03-19 12:05:17 +0300

    done

---
+++ Exercise 1/ex1.js
@@ -0,0 +1,10 @@
+(function travel(x=mars) {
+   
+    console.log(
+        "i want to travel to" + x)
+   
+})
+(uranus)
+let planet = prompt ("what is your favourite planet")
+travel(planet);
+    
