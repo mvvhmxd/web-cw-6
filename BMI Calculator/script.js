5920cfe5dcf4e1fa1558c88456f539445cb23034
Mahmoud Mohamed Balash <mbh27176@gmail.com>
2022-03-21 18:21:05 +0300

    s

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,2 +1,37 @@
 // هنا سوف تنشئ دالة calculate 
-function calculate 
+
+
+
+function calculate(){
+    let height =document.getElementById("height").value;
+    let weight =document.getElementById("weight").value;
+
+
+
+
+   if(height === '') {
+       alert( "height is required")
+   }
+if(weight === '') {
+    alert( "weight is required")
+}
+
+
+    let bmi = weight/(height=height);
+    let status = " ";
+    if (bmi <18.5)
+    {
+        status= "you are underweight"
+    }else if (bmi<25){
+    status ="your weight is healthy"}
+    if (bmi <30){
+    status="you are over weight"  } 
+    if( bmi > 30){
+        status="you have obesity"
+    }
+
+    bmi = bmi.toFixed(2);
+    document.getElementById("result").innerHTML = bmi;
+document.getElementsByClassName("comment")[0].innerHTML = status;
+
+}
